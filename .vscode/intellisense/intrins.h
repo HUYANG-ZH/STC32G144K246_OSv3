1a9093d05ecfeae417f598d681dd092cc0893893
HUYANG <huyangzhang246@gmail.com>
2026-05-11 19:56:51 +0800

    首次提交，将整个库改为UTF8编码

---
+++ .vscode/intellisense/intrins.h
@@ -0,0 +1,14 @@
+#ifndef __VSCODE_INTRINS_H__
+#define __VSCODE_INTRINS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline void _nop_(void) {}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ .vscode/intellisense/keil_c251_intellisense.h
@@ -0,0 +1,12 @@
+#ifndef __VSCODE_KEIL_C251_INTELLISENSE_H__
+#define __VSCODE_KEIL_C251_INTELLISENSE_H__
+
+#ifndef __C251__
+#define __C251__ 1
+#endif
+
+#ifndef __C51__
+#define __C51__ 1
+#endif
+
+#endif
