c2148f3cb94902f1354829b21da7e9e5d1e53f64
ZHY <huyangzhang246@gmail.com>
2026-05-24 15:58:01 +0800

    载荷分配硬件浮点化，极性未确定，加入了限幅

--- STC32G144K246_100Pin_Library/Seekfree_STC32G144K_100Pin_Opensource_Library/project/L3_APP/app_load_distribution.c
+++ STC32G144K246_100Pin_Library/Seekfree_STC32G144K_100Pin_Opensource_Library/project/L3_APP/app_load_distribution.c
@@ -1,23 +1,57 @@
 #include "zf_common_headfile.h"
-#include "service_packet.h"
-#include "app_feedforward.h"
-#include "app_motion_preprocess.h"
+#include "sys_tfpu.h"
 #include "app_load_distribution.h"
 
-#define CAR_WIDTH_M                                   (0.15f)     // 车辆宽度，单位 m
-#define CAR_HEIGHT_M                                  (0.05f)     // 车辆重心高度，单位 m
-#define GRAVITY_MPS2                                  (3 * 9.81f)     // 实际重力加速度，单位 m/s^2
+#define APP_LOAD_DISTRIBUTION_CAR_WIDTH_M            (0.15f)       // 车辆宽度，单位 m
+#define APP_LOAD_DISTRIBUTION_CAR_HEIGHT_M           (0.05f)       // 车辆重心高度，单位 m
+#define APP_LOAD_DISTRIBUTION_GRAVITY_MPS2           (29.43f)      // 实际重力加速度，单位 m/s^2
+#define APP_LOAD_DISTRIBUTION_DEG_TO_RAD             (0.01745f)    // 角度转弧度系数
+#define APP_LOAD_DISTRIBUTION_LTC_LIMIT              (0.5f)        // 荷载转移比例限幅
+
+static float app_load_distribution_limit_ltc(float ltc)
+{
+    float ltc_min;
+
+    if(ltc > APP_LOAD_DISTRIBUTION_LTC_LIMIT)
+    {
+        return APP_LOAD_DISTRIBUTION_LTC_LIMIT;
+    }
+
+    ltc_min = tfpu_sub(0.0f, APP_LOAD_DISTRIBUTION_LTC_LIMIT);
+    if(ltc < ltc_min)
+    {
+        return ltc_min;
+    }
+
+    return ltc;
+}
 
 void app_load_distribution_init(void)
 {
-    
 }
 
 void app_load_distribution_process(app_load_distribution_data_t *datas)
 {
-    datas->ay = datas->straight_current * (datas->turn_current * 0.01745f);
-    datas->LTC = (datas->ay * CAR_HEIGHT_M) / (GRAVITY_MPS2 * CAR_WIDTH_M);
-    // 此处极性未确定
-    datas->load_left = datas->Target_differential_speed * (0.5 + datas->LTC);
-    datas->load_right = datas->Target_differential_speed * (0.5 + datas->LTC);
+    float turn_radps;
+    float ltc_denominator;
+    float left_ratio;
+    float right_ratio;
+
+    if(NULL == datas)
+    {
+        return;
+    }
+
+    turn_radps = tfpu_mul(datas->turn_current, APP_LOAD_DISTRIBUTION_DEG_TO_RAD);
+    datas->ay = tfpu_mul(datas->straight_current, turn_radps);
+
+    ltc_denominator = tfpu_mul(APP_LOAD_DISTRIBUTION_GRAVITY_MPS2, APP_LOAD_DISTRIBUTION_CAR_WIDTH_M);
+    datas->LTC = tfpu_div(tfpu_mul(datas->ay, APP_LOAD_DISTRIBUTION_CAR_HEIGHT_M), ltc_denominator);
+    datas->LTC = app_load_distribution_limit_ltc(datas->LTC);
+
+    left_ratio = tfpu_sub(0.5f, datas->LTC);
+    right_ratio = tfpu_add(0.5f, datas->LTC);
+
+    datas->load_left = tfpu_mul(datas->Target_differential_speed, left_ratio);
+    datas->load_right = tfpu_mul(datas->Target_differential_speed, right_ratio);
 }
