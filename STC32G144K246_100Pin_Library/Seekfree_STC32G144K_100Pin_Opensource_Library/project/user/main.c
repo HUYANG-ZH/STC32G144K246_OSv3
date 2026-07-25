53913a26758c723c0c00e920272062e3d4d28acb
HUYANG <huyangzhang246@gmail.com>
2026-07-25 15:24:54 +0800

    开始测试重构内容

--- STC32G144K246_100Pin_Library/Seekfree_STC32G144K_100Pin_Opensource_Library/project/user/main.c
+++ STC32G144K246_100Pin_Library/Seekfree_STC32G144K_100Pin_Opensource_Library/project/user/main.c
@@ -1,22 +1,61 @@
 #include "zf_common_headfile.h"
 #include "sys_include.h"
 #include "bsp_buzzer.h"
+#include "service_timetick.h"
+#include "service_function_queue.h"
+#include "service_wireless_uart.h"
+#include "service_packet.h"
+#include "service_batterycheck.h"
+#include "service_buzzer.h"
+#include "service_imu.h"
+#include "service_inductor.h"
+#include "service_motor.h"
+#include "service_negative_pressure.h"
+#include "service_speed.h"
+#include "service_tof.h"
+#include "app_element.h"
+#include "app_feedforward.h"
+#include "app_inductor_preprocess.h"
+#include "app_motion_preprocess.h"
+#include "app_motion_postprocess.h"
+#include "app_scheduler.h"
+#include "app_speed_plan.h"
+#include "app_speedout.h"
+#include "app_boot_sequence.h"
 #include "service_boot_request.h"
 
 #define BOOT_BUZZ_MS            (500UL)
-#define PERIOD_BUZZ_MS          (500UL)
-#define PERIOD_ON_MS            (20UL)
-#define PERIOD_OFF_MS           (PERIOD_BUZZ_MS - PERIOD_ON_MS)
 
 void main(void)
 {
     SystemStart();
 
-    bsp_buzzer_init();
+    service_timetick_init();
+    service_function_queue_init();
+    app_scheduler_init();
+    service_wireless_uart_init();
+    service_packet_init();
+    service_batterycheck_init();
+    service_buzzer_init();
+    service_buzzer_stop();
+    service_imu_init();
+    service_tof_init();
+    service_motor_init();
+    service_negative_pressure_init();
+    service_speed_init();
+    app_inductor_preprocess_init();
+    app_motion_preprocess_init();
+    app_feedforward_init();
+    app_speed_plan_init();
+    app_element_init();
+    app_speedout_init();
+    app_motion_postprocess_init();
+    app_boot_sequence_init();
     service_boot_request_init();
 
     wdt_init();
 
+    bsp_buzzer_init();
     bsp_buzzer_on();
     system_delay_ms(BOOT_BUZZ_MS);
     bsp_buzzer_off();
@@ -24,11 +63,17 @@ void main(void)
     while(1)
     {
         wdt_feed();
+        iic_async_process_all_timed(service_timetick_what());
+        service_imu_task();
+        service_tof_task();
+        service_batterycheck_task();
+        service_inductor_task();
         service_boot_request_process();
-
-        bsp_buzzer_on();
-        system_delay_ms(PERIOD_ON_MS);
-        bsp_buzzer_off();
-        system_delay_ms(PERIOD_OFF_MS);
+        service_function_queue_update();
+        service_packet_update();
+        app_scheduler_run();
+        app_element_pump_events();
+        service_negative_pressure_task();
+        service_buzzer_task();
     }
 }
